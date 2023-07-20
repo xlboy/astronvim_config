3ddbf98520d3254328a73e73da3bede4b69771ad
rea <hi@rea.ink>
2023-07-20 10:41:42 +0800

    add: treesitter subject, surround

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "RRethy/nvim-treesitter-textsubjects",
+  },
   opts = function(_, opts)
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
@@ -7,5 +10,23 @@ return {
       "scss",
       "vim",
     })
+    opts.incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<CR>",
+        node_incremental = "<CR>",
+        node_decremental = "<BS>",
+        scope_incremental = "<TAB>",
+      },
+    }
+    opts.textsubjects = {
+      enable = true,
+      prev_selection = ",",
+      keymaps = {
+        ["."] = "textsubjects-smart",
+        [";"] = "textsubjects-container-outer",
+        ["i;"] = "textsubjects-container-inner",
+      },
+    }
   end,
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -62,4 +62,27 @@ return {
     },
     config = true,
   },
+  {
+    "echasnovski/mini.surround",
+    keys = {
+      { "gza", desc = "Add surrounding",                     mode = { "n", "v" } },
+      { "gzd", desc = "Delete surrounding" },
+      { "gzf", desc = "Find right surrounding" },
+      { "gzF", desc = "Find left surrounding" },
+      { "gzh", desc = "Highlight surrounding" },
+      { "gzr", desc = "Replace surrounding" },
+      { "gzn", desc = "Update `MiniSurround.config.n_lines`" },
+    },
+    opts = {
+      mappings = {
+        add = "gza",
+        delete = "gzd",
+        find = "gzf",
+        find_left = "gzF",
+        highlight = "gzh",
+        replace = "gzr",
+        update_n_lines = "gzn",
+      },
+    },
+  },
 }
