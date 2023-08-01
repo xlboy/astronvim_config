93c7b24240d3d6af72ab15fddbc93482b12936c3
xlboy <xlboy88888@foxmail.com>
2023-08-01 23:07:50 +0800

    1

--- plugins/user.lua
+++ plugins/user.lua
@@ -116,8 +116,8 @@ return {
       },
     },
     keys = {
-      { "S", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-      { "s", mode = { "n", "x", "o" }, function()
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function()
         require("flash").jump{
           search = { mode = "fuzzy" }
         }
@@ -200,5 +200,45 @@ return {
         default_mappings = true
       }
     end
+  },
+  {
+    'tomasky/bookmarks.nvim',
+  -- after = "telescope.nvim",
+    event = "VeryLazy",
+    config = function()
+      vim.opt.signcolumn = "yes:2"
+      require("bookmarks").setup({
+        save_file = vim.fn.expand("$HOME/.local/share/nvim/bookmarks"), -- bookmarks save file path
+        keywords = {
+          ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+          ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+          ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+          ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+        },
+        on_attach = function(bufnr)
+          local bm = require("bookmarks")
+          local map = vim.keymap.set
+          map("n", "mm", bm.bookmark_toggle) -- add or remove bookmark at current line
+          map("n", "mi", bm.bookmark_ann) -- add or edit mark annotation at current line
+          map("n", "mc", bm.bookmark_clean) -- clean all marks in local buffer
+          map("n", "mn", bm.bookmark_next) -- jump to next mark in local buffer
+          map("n", "mp", bm.bookmark_prev) -- jump to previous mark in local buffer
+          map("n", "ml", bm.bookmark_list) -- show marked file list in quickfix window
+        end,
+      })
+    end
+  },
+  {
+    "dnlhc/glance.nvim",
+    event = "VeryLazy",
+    config = function()
+      require('glance').setup({
+      -- your configuration
+      })
+      vim.keymap.set("n", "gD", "<CMD>Glance definitions<CR>")
+      vim.keymap.set("n", "gR", "<CMD>Glance references<CR>")
+      vim.keymap.set("n", "gY", "<CMD>Glance type_definitions<CR>")
+      vim.keymap.set("n", "gM", "<CMD>Glance implementations<CR>")
+    end,
   }
 }
