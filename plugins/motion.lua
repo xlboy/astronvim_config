0e3e5092dc8dd1ac0e9d65acac738fdd6980527b
xlboy <xlboy88888@foxmail.com>
2023-08-19 18:05:53 +0800

    1

--- plugins/motion.lua
+++ plugins/motion.lua
@@ -14,46 +14,57 @@ return {
       },
     },
     keys = {
-      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
       {
         "S",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump {
+          require("flash").jump({
             search = { mode = "fuzzy" },
-          }
+          })
         end,
       },
       {
         "<leader>ssv",
         mode = { "n", "o", "x" },
-        function() require("flash").treesitter_search() end,
+        function()
+          require("flash").treesitter_search()
+        end,
       },
       {
         "<leader>sv",
         mode = { "n", "o", "x" },
-        function() require("flash").treesitter() end,
+        function()
+          require("flash").treesitter()
+        end,
       },
       {
         "<leader>k",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump {
+          require("flash").jump({
             search = { mode = "search", max_length = 0, forward = false, wrap = false, multi_window = false },
             label = { after = { 0, 0 } },
             pattern = "^",
-          }
+          })
         end,
       },
       {
         "<leader>j",
         mode = { "n", "x", "o" },
         function()
-          require("flash").jump {
+          require("flash").jump({
             search = { mode = "search", max_length = 0, forward = true, wrap = false, multi_window = false },
             label = { after = { 0, 0 } },
             pattern = "^",
-          }
+          })
         end,
       },
     },
@@ -61,6 +72,30 @@ return {
   {
     "chaoren/vim-wordmotion",
     event = "VeryLazy",
-    init = function() vim.g.wordmotion_prefix = ';' end,
+    init = function()
+      vim.g.wordmotion_prefix = ";"
+    end,
+  },
+  {
+    "ggandor/flit.nvim",
+    keys = function()
+      ---@type LazyKeys[]
+      local ret = {}
+      for _, key in ipairs({ "f", "F", "t", "T" }) do
+        ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+      end
+      return ret
+    end,
+    opts = {
+      labeled_modes = "nx",
+      multiline = false,
+      opts = {},
+    },
+    dependencies = {
+      "ggandor/leap.nvim",
+      dependencies = {
+        "tpope/vim-repeat",
+      },
+    },
   },
 }
