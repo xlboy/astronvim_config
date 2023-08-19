f4a8dd9346b7704e9fab97ba1ef87f7b2100056d
xlboy <xlboy88888@foxmail.com>
2023-08-19 14:47:07 +0800

    1

---
+++ plugins/index.lua
@@ -0,0 +1,9 @@
+return {
+  require "user.plugins.astro.community",
+  require "user.plugins.astro.default",
+
+  require "user.plugins.language.mason",
+  require "user.plugins.language.null-ls",
+  require "user.plugins.language.treesitter",
+  require "user.plugins.language.lsp-helpers",
+}
