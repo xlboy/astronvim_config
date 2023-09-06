3ca7b7cc36538733bf253a08d31ce259c3c0067f
xlboy <xlboy88888@foxmail.com>
2023-09-06 08:57:09 +0800

    1

--- plugins/language/treesitter.lua
+++ plugins/language/treesitter.lua
@@ -1,34 +1,60 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "RRethy/nvim-treesitter-textsubjects",
-    "Eandrju/cellular-automaton.nvim",
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "RRethy/nvim-treesitter-textsubjects",
+      "Eandrju/cellular-automaton.nvim",
+    },
+    opts = function(_, opts)
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "lua",
+        "bash",
+        "scss",
+        "vim",
+        "markdown",
+      })
+      opts.incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          node_incremental = "<CR>",
+          node_decremental = "<BS>",
+          scope_incremental = "<TAB>",
+        },
+      }
+      opts.textsubjects = {
+        enable = true,
+        prev_selection = ",",
+        keymaps = {
+          ["."] = "textsubjects-smart",
+          [";"] = "textsubjects-container-outer",
+          ["i;"] = "textsubjects-container-inner",
+        },
+      }
+    end,
   },
-  opts = function(_, opts)
-    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "bash",
-      "scss",
-      "vim",
-      "markdown"
-    })
-    opts.incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<CR>",
-        node_incremental = "<CR>",
-        node_decremental = "<BS>",
-        scope_incremental = "<TAB>",
-      },
-    }
-    opts.textsubjects = {
-      enable = true,
-      prev_selection = ",",
-      keymaps = {
-        ["."] = "textsubjects-smart",
-        [";"] = "textsubjects-container-outer",
-        ["i;"] = "textsubjects-container-inner",
-      },
-    }
-  end
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      local tscontext = require("treesitter-context")
+      tscontext.setup({
+        enable = true,
+        max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit
+        min_window_height = 5, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+    end,
+  },
 }
