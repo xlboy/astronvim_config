8301e05d2b26544572d0007033e5ee576fe7eb87
xlboy <xlboy88888@foxmail.com>
2023-09-08 18:02:58 +0800

    1

--- plugins/language/cmp.lua
+++ plugins/language/cmp.lua
@@ -67,8 +67,8 @@ return {
       },
       sources = cmp.config.sources({
         { name = "nvim_lsp", priority = 1000 },
-        { name = "luasnip", priority = 750 },
-        { name = "buffer", priority = 500 },
+        -- { name = "luasnip", priority = 750 },
+        -- { name = "buffer", priority = 500 },
         { name = "path", priority = 250 },
       }),
     }
