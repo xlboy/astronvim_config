bf52b27dfecf826bdcaec4353f9bbe77f05cdb9b
xlboy <xlboy88888@foxmail.com>
2023-10-11 18:36:16 +0800

    1

--- plugins/experience.lua
+++ plugins/experience.lua
@@ -76,4 +76,32 @@ return {
       require("femaco").setup()
     end,
   },
+  {
+    "Wansmer/symbol-usage.nvim",
+    event = "BufReadPre",
+    config = function()
+      require("symbol-usage").setup({
+        vt_position = "end_of_line",
+        text_format = function(symbol)
+          local fragments = {}
+
+          if symbol.references then
+            local usage = symbol.references <= 1 and "" or "usages"
+            local num = symbol.references == 0 and "" or symbol.references
+            table.insert(fragments, ("%s %s"):format(num, usage))
+          end
+
+          if symbol.definition then
+            table.insert(fragments, symbol.definition .. " defs")
+          end
+
+          if symbol.implementation then
+            table.insert(fragments, symbol.implementation .. " impls")
+          end
+
+          return table.concat(fragments, ", ")
+        end,
+      })
+    end,
+  },
 }
--- plugins/utility.lua
+++ plugins/utility.lua
@@ -63,4 +63,19 @@ return {
       })
     end,
   },
+  {
+    "sontungexpt/url-open",
+    event = "VeryLazy",
+    cmd = "URLOpenUnderCursor",
+    keys = {
+      { "<leader>uo", "<CMD>URLOpenUnderCursor<CR>", mode = { "n" } },
+    },
+    config = function()
+      local status_ok, url_open = pcall(require, "url-open")
+      if not status_ok then
+        return
+      end
+      url_open.setup({})
+    end,
+  },
 }
