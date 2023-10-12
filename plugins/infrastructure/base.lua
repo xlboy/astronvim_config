43d2621b451293cd65a3ef14e0b581d2fede715f
xlboy <xlboy88888@foxmail.com>
2023-10-12 17:11:23 +0800

    remove xlboy/project, auto-session

--- plugins/infrastructure/base.lua
+++ plugins/infrastructure/base.lua
@@ -35,39 +35,4 @@ return {
     lazy = false,
     priority = 100,
   },
-  -- {
-  --   "xlboy/project.nvim",
-  --   -- dir = "~/Desktop/xlboy/__open-source__/project.nvim",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     require("project_nvim").setup({
-  --       manual_mode = true,
-  --       ignore_lsp = { "lua_ls" },
-  --       patterns = { ".git", "Makefile", "pnpm-workspace.yaml", "yarn.lock", "pnpm-lock.yaml" },
-  --     })
-  --     require("telescope").load_extension("projects")
-  --   end,
-  -- },
-  -- {
-  --   "rmagatti/auto-session",
-  --   event = "VeryLazy",
-  --   config = function()
-  --     vim.o.sessionoptions = "buffers"
-  --     require("auto-session").setup({
-  --       auto_session_suppress_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-  --     })
-  --     vim.api.nvim_create_autocmd("DirChanged", {
-  --       callback = function()
-  --         vim.loop.new_timer():start(
-  --           50,
-  --           0,
-  --           vim.schedule_wrap(function()
-  --             vim.cmd("SessionRestore")
-  --           end)
-  --         )
-  --       end,
-  --       pattern = "global",
-  --     })
-  --   end,
-  -- },
 }
