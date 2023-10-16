cb72ba2490df78c3ded39760e069b6b9b240dfc5
xlboy <xlboy88888@foxmail.com>
2023-10-16 16:05:09 +0800

    add markdown-preview.nvim

--- plugins/infrastructure/base.lua
+++ plugins/infrastructure/base.lua
@@ -32,4 +32,13 @@ return {
       { "Shatur/neovim-session-manager" },
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && yarn install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
